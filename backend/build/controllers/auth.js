8b28b173bd066b67e8a1b974634eaa0976f41387
AndersonN031 <devandersonnunes078@gmail.com>
2024-10-17 09:16:57 -0300

    teste10

--- backend/build/controllers/auth.js
+++ backend/build/controllers/auth.js
@@ -22,28 +22,19 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.login = exports.signup = void 0;
 const bcrypt_1 = require("bcrypt");
 const jwt = __importStar(require("jsonwebtoken"));
 const secrets_1 = require("../secrets");
 const api_1 = require("../api");
-const signup = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const signup = async (req, res) => {
     const { name, email, password } = req.body;
-    let user = yield api_1.prismaClient.user.findFirst({ where: { email } });
+    let user = await api_1.prismaClient.user.findFirst({ where: { email } });
     if (user) {
         throw Error('Usuário já existe!');
     }
-    user = yield api_1.prismaClient.user.create({
+    user = await api_1.prismaClient.user.create({
         data: {
             name,
             email,
@@ -51,11 +42,11 @@ const signup = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         }
     });
     res.json(user);
-});
+};
 exports.signup = signup;
-const login = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const login = async (req, res) => {
     const { email, password } = req.body;
-    let user = yield api_1.prismaClient.user.findFirst({ where: { email } });
+    let user = await api_1.prismaClient.user.findFirst({ where: { email } });
     if (!user) {
         throw Error('Esse usuário não existe!');
     }
@@ -66,5 +57,5 @@ const login = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         userId: user.id,
     }, secrets_1.JWT_SECRET);
     res.json({ user, token });
-});
+};
 exports.login = login;
